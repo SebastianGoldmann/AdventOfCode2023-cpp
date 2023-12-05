2aa365ccfc38e14bfa11f89376f63b3f41a0cd24
Sebastian <sebastian.h.goldmann@outlook.com>
2023-12-05 21:37:09 +0100

    assinment 1B done

--- aoc2023/Assignment_1.cpp
+++ aoc2023/Assignment_1.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <list>
 #include <map>
+#include <vector>
+#include <string>
+#include <map>
+
 
 // Define a macro to read and print the contents of a text file
 std::vector<std::string> read_txt_file(const std::string& file_name) {
@@ -32,19 +36,43 @@ std::vector<std::string> read_txt_file(const std::string& file_name) {
     return lines;
 }
 
+std::string correct_string(std::string line){
 
+    std::map<std::string, std::string> possible_names;
+    possible_names = {{"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"}, {"five", "5"},{"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}};
 
-int replace_numbers(std::vector<std::string> lines){
+    for (const auto& string_and_numb : possible_names){
+        
+        // find the string index
+        std::size_t found;
+        found = line.find(string_and_numb.first);
 
-    std::map<std::string, int> possible_names;
-    possible_names = {{"one",1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5},{"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+        // Check if all intances were found
+        while (found != std::string::npos){
+            // replace entire word with letter.
+            //line.replace(found, string_and_numb.first.length(), string_and_numb.second);
+            // WE NEED TO INSERT, BC ONEIGHT COULD REPLACE EIGHT FIRST AND THEN ONE DOES NOT COUNT!
+            line.insert(found+2, string_and_numb.second);
+            std::cout << line << std::endl;
+            //find next name string and replace.
+            //found = line.find(string_and_numb.first, found + string_and_numb.second.length());
+            found  = line.find(string_and_numb.first, found + string_and_numb.first.length());
+        }
+    }
+    return line;
+}
 
+std::vector<std::string> replace_numbers_by_int_string(std::vector<std::string> lines){
 
+    // loop through the string and grab each set of 3 and each set of 4.
+    for (std::string& line : lines){
+        std::cout << line << std::endl;    
+        // loop through all possible sting names WRONG FIND FIRST WORD  in string
+        line = correct_string(line);
+    }
+    // return lines to
     
-
-    // return sum to print
-    return sum;
-
+    return lines;
 }
 
 
@@ -55,8 +83,7 @@ int read_outer_numbers(std::vector<std::string> lines){
     int first_int;
     int last_int;
     int final_int;
-    std::vector<int> final_numbers;
-    final_numbers = {};
+    std::vector<int> final_numbers = {};
 
     //loop through lines
     for (const std::string& line : lines) {
@@ -70,7 +97,6 @@ int read_outer_numbers(std::vector<std::string> lines){
 
         // loop through characters in line
         for (char entry : line){
-            
 
             // select first integer 
             if (std::isdigit(entry))
@@ -105,10 +131,6 @@ int read_outer_numbers(std::vector<std::string> lines){
         sum += numb;
     }
 
-    std::cout << "Sum of numbers: " << sum << std::endl;
-    std::cout << "Press Enter to exit...";
-    std::cin.get();
-
     // return sum for print
     return sum;
 }
@@ -117,87 +139,29 @@ int read_outer_numbers(std::vector<std::string> lines){
 
 int main() {
     // Declare variables
-    std::vector<std::string> lines;
+    std::vector<std::string> lines, lines_corrected;
     std::string file_path;
     int sum;
 
 
-    // Read file
+    // Read lines
     file_path  ="C:\\Users\\Gebruiker\\Documents\\coding\\Advent of code 2023\\data\\assigment1_data.txt";
     lines = read_txt_file(file_path);
 
-    //calc ass1 part 1
+    //calc assignment 1 part 1
     sum = read_outer_numbers(lines);
+
     std::cout << "Sum of numbers assignment 1A: " << sum << std::endl;
 
 
     // start part 2 replace all string numbers in the strings with the integer numbers
-    replace_numbers(lines);
-    sum = read_outer_numbers(lines);
+    lines_corrected = replace_numbers_by_int_string(lines);
+    sum = read_outer_numbers(lines_corrected);
+
+
     std::cout << "Sum of numbers assignment 1B: " << sum << std::endl;
     std::cout << "Press Enter to exit...";
     std::cin.get();
 
-
-    // // declare vars
-    // bool first_int_bool;
-    // bool last_int_bool;
-    // int first_int;
-    // int last_int;
-    // int final_int;
-    // std::vector<int> final_numbers;
-    // final_numbers = {};
-    // //loop through lines
-    // for (const std::string& line : lines) {
-
-    //     first_int_bool = true;
-    //     last_int_bool = true;
-    //     first_int = 0;
-    //     last_int = 0;
-    //     final_int = 0;
-
-
-    //     // loop through characters in line
-    //     for (char entry : line){
-            
-
-    //         // select first integer 
-    //         if (std::isdigit(entry))
-    //             if (first_int_bool){
-    //                 first_int_bool=false;
-    //                 first_int = entry-'0';
-    //             }
-
-    //         //select last integer
-    //         else if(std::isdigit(entry)){
-    //             last_int_bool = false;
-    //             last_int = entry-'0';
-                
-    //         }
-    //     }
-    //     if (last_int_bool){
-    //         //there is only 1 char in the string
-    //         last_int = first_int;
-    //     }
-
-    //     // save first and last integer as number
-    //     final_int = first_int*10 + last_int;
-        
-    //     // append final number
-    //     final_numbers.push_back(final_int);
-    // }
-
-    // //sum numbers
-    // int sum;
-    // sum = 0;
-    // for (int numb: final_numbers){
-    //     sum += numb;
-    // }
-
-    std::cout << "Sum of numbers: " << sum << std::endl;
-    std::cout << "Press Enter to exit...";
-    std::cin.get();
-
-    // Return 0 to indicate successful execution
     return 0;
 }
